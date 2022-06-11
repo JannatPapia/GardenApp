c7cf4ff198dab3bfc0b7e171973c796eecd1b9c1
PapiaMCode <papiamcode@gmail.com>
2022-06-10 22:48:50 -0700

    Initial Commit

---
+++ GradeningApp/GradeningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradeningAppApp.swift
+//  GradeningApp
+//
+//  Created by Jannatun Nahar Papia on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GradeningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
